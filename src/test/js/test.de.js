3421e109d8c210bf1277d9c56611dabdb286934c
Ralf Hergert <ralf.hergert@gmx.de>
2015-07-28 00:34:18 +0200

    fixed regular composit noun should start with an upper case letter

--- src/test/js/test.de.js
+++ src/test/js/test.de.js
@@ -3,3 +3,8 @@ QUnit.test("test dash-connected compound nouns", function(assert) {
 	var output = spellchecker.check('Internet-Plattform');
 	assert.equal(output, 'Internet-Plattform', "Should not contain any spellcheck spans." );
 });
+QUnit.test("test compound noun", function(assert) {
+	// let the spellchecker run.
+	var output = spellchecker.check('Außenministerium');
+	assert.equal(output, 'Außenministerium', "Should not contain any spellcheck spans." );
+});

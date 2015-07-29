568fe46f57d0d2983ff1f843ec7be0bde70d464f
Ralf Hergert <ralf.hergert@gmx.de>
2015-07-29 21:33:02 +0200

    fixed case for compound nouns with names or abbreviations like 'EU-Kommission'

--- src/test/js/test.de.js
+++ src/test/js/test.de.js
@@ -8,3 +8,8 @@ QUnit.test("test compound noun", function(assert) {
 	var output = spellchecker.check('Außenministerium');
 	assert.equal(output, 'Außenministerium', "Should not contain any spellcheck spans." );
 });
+QUnit.test("test composit of name and noun", function(assert) {
+	// let the spellchecker run.
+	var output = spellchecker.check('EU-Kommission');
+	assert.equal(output, 'EU-Kommission', "Should not contain any spellcheck spans." );
+});

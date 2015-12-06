5b7268b1065597b1bc11e4b1e9ed5db2d436080e
Ralf Hergert <ralf.hergert@gmx.de>
2015-12-06 22:49:32 +0100

    added a test against non existing word

--- src/test/js/test.de.js
+++ src/test/js/test.de.js
@@ -289,3 +289,8 @@ QUnit.test("test against Aufgrund", function(assert) {
 	var output = spellchecker.checkText('Aufgrund', {hyphenation: false});
 	assert.notEqual(output, 'Aufgrund', "a case warning must be given, since aufgrund is an adverb and must be written in lower case");
 });
+QUnit.test("test against Kräfteeinteilen", function(assert) {
+	spellchecker.setAssumeStartOfSentence(false);
+	var output = spellchecker.checkText('Sie werden sich ihre Kräfteeinteilen müssen', {hyphenation: false});
+	assert.notEqual(output, 'Sie werden sich ihre Kräfteeinteilen müssen', "a case warning is expected, since 'Kräfteinteilen' is not a valid word");
+});
